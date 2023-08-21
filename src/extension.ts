a1ed114157b0339d7c7a9adfdfccd13ea32166cf
Adam Fowler <adamfowler71@gmail.com>
2023-08-21 17:27:56 +0100

    Added LLDBDebugAdapterExecutableFactory

--- src/extension.ts
+++ src/extension.ts
@@ -11,13 +11,31 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('"vscode-swift-lldb" is now active!');
 
 	if (useServer) {
-		const factory = new LLDBDebugServerDescriptorFactory();
+		const factory = new LLDBDebugAdapterExecutableFactory();
 		context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('swift-lldb', factory));
 	}
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+class LLDBDebugAdapterExecutableFactory implements vscode.DebugAdapterDescriptorFactory {
+
+	// The following use of a DebugAdapter factory shows how to control what debug adapter executable is used.
+	// Since the code implements the default behavior, it is absolutely not neccessary and we show it here only for educational purpose.
+
+	createDebugAdapterDescriptor(_session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+		// param "executable" contains the executable optionally specified in the package.json (if any)
+
+		// use the executable specified in the package.json if it exists or determine it based on some other information (e.g. the session)
+		if (!executable) {
+			executable = new vscode.DebugAdapterExecutable(getSwiftExecutable("lldb-vscode"), [], {});
+		}
+
+		// make VS Code launch the DA executable
+		return executable;
+	}
+}
 
 class LLDBDebugServerDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
 
